c24350c40b6c6cdee4fae6095c10b224432b1f44
Jean M. Lescure <jeanmlescure@gmail.com>
2021-08-16 19:35:06 -0600

    chore: add tests

---
+++ src/test.ts
@@ -0,0 +1,55 @@
+import NanoAudit from ".";
+
+const auditName = 'test';
+
+const {audit} = new NanoAudit(auditName);
+
+const firstFn = () => {
+  const auditResult = audit(auditName, `firstFn`);
+  const startedAt = Date.now();
+
+  while ((Date.now() - startedAt) < 1000) {}
+
+  auditResult.end();
+};
+
+const secondFn = () => {
+  const auditResult = audit(auditName, 'secondFn');
+
+  firstFn();
+  auditResult.end();
+};
+
+const thirdFn = () => {
+  const auditResult = audit(auditName, 'thirdFn');
+
+  secondFn();
+  auditResult.end();
+};
+
+const fourthFn = () => {
+  const auditResult = audit(auditName, `fourthFn`);
+  const startedAt = Date.now();
+
+  setTimeout(() => {
+    while ((Date.now() - startedAt) < 1000) {}
+
+    auditResult.end();
+  });
+};
+
+setTimeout(firstFn, 100);
+setTimeout(firstFn, 100);
+setTimeout(firstFn, 100);
+setTimeout(firstFn, 100);
+
+secondFn();
+
+[...Array(99).keys()].map((n) => audit(auditName, `xFn${n}`)).forEach((a) => a.end());
+
+thirdFn();
+
+setTimeout(fourthFn, 100);
+setTimeout(fourthFn, 200);
+setTimeout(fourthFn, 300);
+setTimeout(fourthFn, 400);
